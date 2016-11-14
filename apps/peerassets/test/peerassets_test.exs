f6a72f0f823306c7d3e46d0f5245aff9899314f8
hrobeers <hansrobeers@gmail.com>
2016-11-14 20:23:35 +0100

    Test PeerAssets deck spawn meta-data decoding

--- apps/peerassets/test/peerassets_test.exs
+++ apps/peerassets/test/peerassets_test.exs
@@ -4,6 +4,7 @@ defmodule Mercator.PeerAssetsTest do
 
   alias Mercator.PeerAssets.Repo
   alias Bitcoin.Protocol.Types.Script
+  alias Mercator.PeerAssets.Protobufs
 
   test "P2TH import" do
     Application.get_env(:peerassets, :PAprod)
@@ -22,6 +23,7 @@ defmodule Mercator.PeerAssetsTest do
   end
 
   test "Decode deck spawn txn" do
+    # Decoding the PeerAssets challenge transaction: https://www.peercointalk.org/index.php?topic=4760.0
     txn = Mercator.RPC.gettransaction!("356b9736ee7dbf387ea7b10a16beda8ea1ad5db0cbc53e749f5e4b3cf7545552")
     # Assert this can be a deck spawn txn
     assert txn.outputs |> Enum.count > 2
@@ -31,5 +33,18 @@ defmodule Mercator.PeerAssetsTest do
     |> Enum.at(0)
     |> Map.get(:pk_script)
     |> Script.parse_p2pkh! # throws on parse failure
+    # TODO assert sender
+
+    # Parse the second output (OP_RETURN PeerAssets data)
+    pa_data = txn.outputs
+    |> Enum.at(1)
+    |> Map.get(:pk_script)
+    |> Script.parse_opreturn! # throws on parse failure
+    |> Protobufs.DeckSpawn.decode
+    assert pa_data.version == 1
+    assert pa_data.issue_mode == 10
+    assert pa_data.number_of_decimals == 2
+    assert pa_data.short_name == "hrobeers owes you 100PPC, real ones!" # Sorry, it's claimed by saeveritt
+    assert pa_data.asset_specific_data == nil
   end
 end
