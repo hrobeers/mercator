a23379f3a5e492c6529414b0ab6db6d09707a304
hrobeers <hansrobeers@gmail.com>
2016-11-18 08:38:08 +0100

    DeckSpawn type with parse_txn! function

---
+++ apps/peerassets/lib/types/deck_spawn.ex
@@ -0,0 +1,48 @@
+defmodule Mercator.PeerAssets.Types.DeckSpawn do
+  use Bitwise
+
+  alias Bitcoin.Protocol.Types.Script
+  alias Mercator.PeerAssets.Protobufs
+  alias BitcoinTool.Address
+  alias Mercator.PeerAssets.Protobufs.DeckSpawn.MODE
+
+  defstruct owner_address: nil,
+            tag_address: nil,
+            issue_modes: nil,
+            number_of_decimals: nil,
+            short_name: nil,
+            asset_specific_data: nil
+
+  def parse_txn!(txn) do
+    [owner_input | _] = txn.inputs
+    [p2th_output, pa_data_output | _] = txn.outputs
+
+    # Parse the first input (from owner address)
+    owner_address = owner_input
+    |> Script.parse_address! # throws on parse failure
+    |> Address.base58check
+
+    # Parse the first output (P2TH)
+    p2th_address = p2th_output
+    |> Script.parse_address! # throws on parse failure
+    |> Address.base58check
+
+    # Parse the second output (OP_RETURN PeerAssets data)
+    pa_data = pa_data_output
+    |> Script.parse_opreturn! # throws on parse failure
+    |> Protobufs.DeckSpawn.decode
+
+    %Mercator.PeerAssets.Types.DeckSpawn{
+      owner_address: owner_address,
+      tag_address: p2th_address,
+      issue_modes: parse_modes!(pa_data.issue_mode),
+      number_of_decimals: pa_data.number_of_decimals,
+      short_name: pa_data.short_name,
+      asset_specific_data: pa_data.asset_specific_data
+    }
+  end
+
+  defp parse_modes!(issue_mode) do
+    for mode <- MODE.atoms, (issue_mode &&& MODE.value(mode)) > 0, do: mode
+  end
+end
--- apps/peerassets/test/peerassets_test.exs
+++ apps/peerassets/test/peerassets_test.exs
@@ -3,9 +3,7 @@ defmodule Mercator.PeerAssetsTest do
   doctest Mercator.PeerAssets
 
   alias Mercator.PeerAssets.Repo
-  alias Bitcoin.Protocol.Types.Script
-  alias Mercator.PeerAssets.Protobufs
-  alias BitcoinTool.Address
+  alias Mercator.PeerAssets.Types.DeckSpawn
 
   test "P2TH import" do
     Application.get_env(:peerassets, :PAprod)
@@ -27,29 +25,14 @@ defmodule Mercator.PeerAssetsTest do
     # Decoding the PeerAssets challenge transaction: https://www.peercointalk.org/index.php?topic=4760.0
     txn = Mercator.RPC.gettransaction!("356b9736ee7dbf387ea7b10a16beda8ea1ad5db0cbc53e749f5e4b3cf7545552")
 
-    [owner_input] = txn.inputs
-    [p2th_output, pa_data_output, _] = txn.outputs
+    deck_spawn = txn |> DeckSpawn.parse_txn!
 
-    # Parse the first input (from owner address)
-    owner_address = owner_input
-    |> Script.parse_address! # throws on parse failure
-    |> Address.base58check
-    assert owner_address == "mpwRGC6URPCvdU4J83YbUzvqmBhKSDXk4p"
+    assert deck_spawn.owner_address == "mpwRGC6URPCvdU4J83YbUzvqmBhKSDXk4p"
+    assert deck_spawn.tag_address == "mwqncWSnzUzouPZcLQWcLTPuSVq3rSiAAa" # PAtest address on testnet
 
-    # Parse the first output (P2TH)
-    p2th_address = p2th_output
-    |> Script.parse_address! # throws on parse failure
-    |> Address.base58check
-    assert p2th_address == "mwqncWSnzUzouPZcLQWcLTPuSVq3rSiAAa" # PAtest address on testnet
-
-    # Parse the second output (OP_RETURN PeerAssets data)
-    pa_data = pa_data_output
-    |> Script.parse_opreturn! # throws on parse failure
-    |> Protobufs.DeckSpawn.decode
-    assert pa_data.version == 1
-    assert pa_data.issue_mode == 10
-    assert pa_data.number_of_decimals == 2
-    assert pa_data.short_name == "hrobeers owes you 100PPC, real ones!" # Sorry, it's claimed by saeveritt
-    assert pa_data.asset_specific_data == nil
+    assert deck_spawn.issue_modes == [:ONCE] # originally [:ONCE, :PEG] but PEG is removed from spec
+    assert deck_spawn.number_of_decimals == 2
+    assert deck_spawn.short_name == "hrobeers owes you 100PPC, real ones!" # Sorry, it's claimed by saeveritt
+    assert deck_spawn.asset_specific_data == nil
   end
 end
