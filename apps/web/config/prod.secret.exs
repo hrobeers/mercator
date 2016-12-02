61186a9acc2b438d8390eae2441e68e0e3fe37da
hrobeers <hansrobeers@gmail.com>
2016-12-02 18:33:38 +0100

    Add prod.secret.exs template

---
+++ apps/web/config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :web, Mercator.Web.Endpoint,
+  secret_key_base: "Hpo/I+tWxk0R0ZTcX4nuhX2VYPUwNj22BJkfKEYWSeerb8vXv7m9/H03U0ZM98vC"
